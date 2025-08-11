e4649bdd411c30eae91154a8325996958dd048a4
GersonHernandez10 <ghernandezlima@middlebury.edu>
2025-08-11 19:12:16 -0400

    more final touches to the models

--- components/Room.jsx
+++ components/Room.jsx
@@ -4,42 +4,15 @@ import { Suspense } from 'react'
 import { useGLTF } from '@react-three/drei'
 import * as THREE from 'three'
 
-/* ===== Gray Room (walls, floor, ceiling) ===== */
-function GrayRoom({ scale = 1 }) {
-  const wallMat = new THREE.MeshStandardMaterial({ color: '#b0b0b0' }) // light gray
+/* ===== Floor Only ===== */
+function FloorOnly({ scale = 1 }) {
   const floorMat = new THREE.MeshStandardMaterial({ color: '#a0a0a0' }) // slightly darker gray
-  const ceilingMat = new THREE.MeshStandardMaterial({ color: '#b0b0b0' })
 
   return (
     <group scale={scale}>
       {/* Floor */}
       <mesh material={floorMat} position={[0, -0.025, 0]}>
         <boxGeometry args={[5, 0.05, 5]} />
-      </mesh>
-
-      {/* Ceiling */}
-      <mesh material={ceilingMat} position={[0, 2.5, 0]}>
-        <boxGeometry args={[5, 0.05, 5]} />
-      </mesh>
-
-      {/* Back wall */}
-      <mesh material={wallMat} position={[0, 1.25, -2.475]}>
-        <boxGeometry args={[5, 2.5, 0.05]} />
-      </mesh>
-
-      {/* Front wall */}
-      <mesh material={wallMat} position={[0, 1.25, 2.475]}>
-        <boxGeometry args={[5, 2.5, 0.05]} />
-      </mesh>
-
-      {/* Left wall */}
-      <mesh material={wallMat} position={[-2.475, 1.25, 0]}>
-        <boxGeometry args={[0.05, 2.5, 5]} />
-      </mesh>
-
-      {/* Right wall */}
-      <mesh material={wallMat} position={[2.475, 1.25, 0]}>
-        <boxGeometry args={[0.05, 2.5, 5]} />
       </mesh>
     </group>
   )
@@ -67,8 +40,8 @@ function SnowboardGLB({ onClick, position, scale = 1 }) {
 }
 function SnowboardFallback({ onClick, position, scale = 1 }) {
   return (
-    <mesh onClick={onClick} position={position} rotation={[0,0,Math.PI/2]} scale={scale}>
-      <boxGeometry args={[0.1,1.4,0.05]} />
+    <mesh onClick={onClick} position={position} rotation={[0, 0, Math.PI / 2]} scale={scale}>
+      <boxGeometry args={[0.1, 1.4, 0.05]} />
       <meshStandardMaterial color="#7dd3fc" />
     </mesh>
   )
@@ -104,12 +77,12 @@ function PalmTreeGLB({ onClick, position, scale = 1 }) {
 function PalmTreeFallback({ onClick, position, scale = 1 }) {
   return (
     <group onClick={onClick} position={position} scale={scale}>
-      <mesh position={[0,0.1,0]}>
-        <cylinderGeometry args={[0.02,0.05,0.6,12]} />
+      <mesh position={[0, 0.1, 0]}>
+        <cylinderGeometry args={[0.02, 0.05, 0.6, 12]} />
         <meshStandardMaterial color="#8b5a2b" />
       </mesh>
-      <mesh position={[0,0.45,0]}>
-        <coneGeometry args={[0.35,0.6,6]} />
+      <mesh position={[0, 0.45, 0]}>
+        <coneGeometry args={[0.35, 0.6, 6]} />
         <meshStandardMaterial color="#22c55e" />
       </mesh>
     </group>
@@ -120,27 +93,27 @@ function PalmTreeFallback({ onClick, position, scale = 1 }) {
 export default function Room({ onComputerClick, onSnowboardClick, onSoccerClick, onBeachClick }) {
   return (
     <group>
-      {/* Gray Room */}
-      <GrayRoom scale={1} />
+      {/* Floor */}
+      <FloorOnly scale={1} />
 
       {/* Desk & PC */}
       <Suspense fallback={null}>
-        <DeskGLB position={[0, 0, 0]} scale={0.7} />
-        <PCGLB position={[0, 0.52, 0.22]} scale={0.03} onClick={onComputerClick} />
+        <DeskGLB position={[0, 0, .07]} scale={0.7} />
+        <PCGLB position={[0, 0.58, 0.000000001]} scale={0.04} onClick={onComputerClick} />
       </Suspense>
 
       {/* Snowboard */}
-      <Suspense fallback={<SnowboardFallback onClick={onSnowboardClick} position={[1.5,0.7,-0.6]} scale={0.9} />}>
+      <Suspense fallback={<SnowboardFallback onClick={onSnowboardClick} position={[1.5, 0.7, -0.6]} scale={0.9} />}>
         <SnowboardGLB onClick={onSnowboardClick} position={[1.5, 0, -0.6]} scale={0.9} />
       </Suspense>
 
       {/* Soccer ball */}
-      <Suspense fallback={<SoccerBallFallback onClick={onSoccerClick} position={[-0.9,0.12,0.4]} scale={0.2} />}>
-        <SoccerBallGLB onClick={onSoccerClick} position={[-0.9, 0.30, 0.4]} scale={0.2} />
+      <Suspense fallback={<SoccerBallFallback onClick={onSoccerClick} position={[-0.9, 0.12, 0.4]} scale={0.2} />}>
+        <SoccerBallGLB onClick={onSoccerClick} position={[-0.9, 0.3, 0.4]} scale={0.2} />
       </Suspense>
 
       {/* Palm tree */}
-      <Suspense fallback={<PalmTreeFallback onClick={onBeachClick} position={[0.9,0,-1.6]} scale={0.2} />}>
+      <Suspense fallback={<PalmTreeFallback onClick={onBeachClick} position={[0.9, 0, -1.6]} scale={0.2} />}>
         <PalmTreeGLB onClick={onBeachClick} position={[0.9, 0, -1.6]} scale={0.2} />
       </Suspense>
     </group>
